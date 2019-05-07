f85358f7ae8a79e714d2ba2ebb0898033a81f0a9
Enel Almonte <enelramon@hotmail.com>
2019-05-06 21:04:53 -0400

    Agregando el ejemplo2

---
+++ Ejemplo2.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ejemplos_Ap1_2019_01
+{
+    public partial class Ejemplo2 : Form
+    {
+        public Ejemplo2()
+        {
+            InitializeComponent();
+        }
+    }
+}
